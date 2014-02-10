167e11aeb8d60d46c76e10e6b2bbb4b1d0489095
11214775 <11214775@F207-15>
2014-02-10 11:25:00 +0100

    Add Class Marin

---
+++ src/org/simo/Modeles/Marin.java
@@ -0,0 +1,36 @@
+package org.simo.Modeles;
+
+public class Marin {
+	
+	private String nom;
+	private String prenom;
+	private int age;
+	private int salaire;
+	public String getNom() {
+		return nom;
+	}
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+	public String getPrenom() {
+		return prenom;
+	}
+	public void setPrenom(String prenom) {
+		this.prenom = prenom;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	public int getSalaire() {
+		return salaire;
+	}
+	public void setSalaire(int salaire) {
+		this.salaire = salaire;
+	}
+	
+	
+
+}
